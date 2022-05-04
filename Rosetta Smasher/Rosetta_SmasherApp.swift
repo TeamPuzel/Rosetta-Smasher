15e431ffcb691bfae0782eada3afe4c6da8fa24e
TeamPuzel <puffed-01.blips@icloud.com>
2022-05-04 14:07:28 +0200

    Initial Commit

---
+++ Rosetta Smasher/Rosetta_SmasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rosetta_SmasherApp.swift
+//  Rosetta Smasher
+//
+//  Created by Wojciech David Wargocki on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rosetta_SmasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
